b83bea62999b0af13c4e0d60f03986a4479acbb6
Bastián Nelson <bastian.nelson@gmail.com>
2022-04-06 20:54:38 -0400

    final final ahora si

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//TOOLTIP
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
